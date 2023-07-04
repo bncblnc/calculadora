acb644c1fba821d624ebeebf8079c1c1ba01fd34
bncblnc <bncblnc@gmail.com>
2023-07-04 11:02:02 -0300

    operations

--- src/App.js
+++ src/App.js
@@ -5,40 +5,53 @@ import { useState } from "react";
 
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState("0");
-  const [firstNumber, setFirstNumber] = useState("0");
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
   const [operation, setOperation] = useState("");
 
   function handleAddNumber(number) {
     setCurrentNumber((prev) => `${prev === "0" ? "" : prev}${number}`);
+    if (firstNumber !== "") setSecondNumber((prev) => `${prev}${number}`);
+  }
+
+  function handleOperation(clicked) {
+    if (firstNumber !== "") return;
+    setOperation(clicked);
+    setFirstNumber(currentNumber);
+    setCurrentNumber((prev) => `${prev} ${clicked} `);
   }
 
   function handleOnClear() {
     setCurrentNumber("0");
-    setFirstNumber("0");
+    reset();
+  }
+
+  function reset() {
+    setFirstNumber("");
+    setSecondNumber("");
     setOperation("");
   }
 
-  function handleSumNumbers() {
-    if (firstNumber === "0") {
-      setFirstNumber(currentNumber);
-      setCurrentNumber("0");
-      setOperation("+");
-    } else {
-      const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum));
-      setOperation("");
-    }
-  }
-
   function handleEquals() {
-    if (!firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
-      switch (operation) {
-        case "+":
-          handleSumNumbers();
-          break;
-        default:
-          break;
-      }
+    switch (operation) {
+      case "+":
+        setCurrentNumber(Number(firstNumber) + Number(secondNumber));
+        reset();
+        break;
+      case "-":
+        setCurrentNumber(Number(firstNumber) - Number(secondNumber));
+        reset();
+        break;
+      case "x":
+        setCurrentNumber(Number(firstNumber) * Number(secondNumber));
+        reset();
+        break;
+      case "÷":
+        setCurrentNumber(Number(firstNumber) / Number(secondNumber));
+        reset();
+        break;
+      default:
+        break;
     }
   }
 
@@ -58,20 +71,20 @@ const App = () => {
           <Button label="4" onClick={() => handleAddNumber("4")} />
           <Button label="5" onClick={() => handleAddNumber("5")} />
           <Button label="6" onClick={() => handleAddNumber("6")} />
-          <Button label="+" onClick={handleSumNumbers} />
+          <Button label="+" onClick={() => handleOperation("+")} />
         </Row>
 
         <Row>
           <Button label="1" onClick={() => handleAddNumber("1")} />
           <Button label="2" onClick={() => handleAddNumber("2")} />
           <Button label="3" onClick={() => handleAddNumber("3")} />
-          <Button label="-" />
+          <Button label="-" onClick={() => handleOperation("-")} />
         </Row>
 
         <Row>
           <Button label="0" onClick={() => handleAddNumber("0")} />
-          <Button label="x" />
-          <Button label="÷" />
+          <Button label="x" onClick={() => handleOperation("x")} />
+          <Button label="÷" onClick={() => handleOperation("÷")} />
           <Button label="=" onClick={handleEquals} />
         </Row>
       </Content>
